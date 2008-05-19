f9582b5c4d15f97236698bf1c331840a0c637826
Nicolas Mérouze <nicolas@ibm-ee8ef2d4e9e.eco-systemes.fr>
2008-05-19 09:48:49 +0200

    Remove Engines dependency

--- init.rb
+++ init.rb
@@ -1,12 +1,14 @@
 Dependencies.load_paths << Rails.root + '/app/presenters'
 
 config.after_initialize do
-  Rails.plugins.each do |plugin|
-    path = Rails.root + "/vendor/plugins/#{plugin.name}/app/presenters"
+  if defined? :Engines
+    Rails.plugins.each do |plugin|
+      path = Rails.root + "/vendor/plugins/#{plugin.name}/app/presenters"
     
-    if File.directory?(path)
-      Dependencies.load_paths << path
-      ActionPresenter.view_paths << path
+      if File.directory?(path)
+        Dependencies.load_paths << path
+        ActionPresenter.view_paths << path
+      end
     end
   end
 end
